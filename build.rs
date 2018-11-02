270bd298fd54a82ed7d4bd1af217dc5554cd9bb1
Dennis Hedegaard <dennis@dhedegaard.dk>
2018-11-02 13:35:35 +0100

    Only depend and use winres if we're building for the windows platform.

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
+#[cfg(windows)]
 extern crate winres;
 
+#[cfg(windows)]
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("icon.ico");
         res.compile().unwrap();
     }
+}
+
+#[cfg(unix)]
+fn main() {
+
 }
